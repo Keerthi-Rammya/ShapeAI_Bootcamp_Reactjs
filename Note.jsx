836a503e69ea3479a9a2f4bccd47c9d8acb17c92
Keerthi-Rammya <85451871+Keerthi-Rammya@users.noreply.github.com>
2021-06-06 22:18:46 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+  return (
+    <div className="note8">
+      <h1>ShapeAI intenships</h1>
+      <p>TShapeAI  provides fullstack web development program. it is really an amezing bootcamp. and it provides two complimentary cource i.e., data analytics and machine learning. </p>
+    </div>
+ );
+  
+}
+export default Note;
